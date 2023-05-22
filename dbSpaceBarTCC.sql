7cf74f618199208d496f0c6a2a4b4af34b71b2f1
IGRT <71153494+IgorTeodoro@users.noreply.github.com>
2023-05-22 20:26:26 -0300

    procedure login

--- dbSpaceBarTCC.sql
+++ dbSpaceBarTCC.sql
@@ -234,3 +234,12 @@ begin
 	from tblUsuario
 	WHERE cod_usuario = @codUsuarioConectado
 end
+GO
+--  procedure login--
+create procedure spacelogin
+	@loguser varchar(20),
+	@senhauser varchar(10)
+As
+Begin
+	Select * from tblUsuario where login_usuario = @loguser and senha_usuario=@senhauser
+END
